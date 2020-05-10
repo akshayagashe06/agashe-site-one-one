e6b90d39b2c144247b1d0b943b877561e0922895
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-09 21:09:56 -0400

    Added react router to project

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "reacr-router-dom";
+import { Link } from "react-router-dom";
 import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
 import "./Home.css";
 
