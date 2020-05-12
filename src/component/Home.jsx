85630b2808e35b6828d483806c87dfed308b5ae8
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-11 20:37:38 -0400

    Added css for Home and Navbar

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,10 +10,21 @@ class Home extends Component {
         <Jumbotron>
           <h2>Welcome to my Website.</h2>
           <p>Lets see how this webiste goes.</p>
+          <Link to="/about">
+            <Button bsStyle="primary"> About</Button>
+          </Link>
         </Jumbotron>
-        <Link to="/about">
-          <Button bsStyle="primary"> About</Button>
-        </Link>
+        <Row className="show-grid text-center">
+          <Col xs={12} sm={4} className="person-wrapper">
+            <Image
+              src="assets/MidtownAtlanta_.jpg"
+              circle
+              className="profile-pic"
+            />
+            <h3>Akshay</h3>
+            <p>This is some text in a column</p>
+          </Col>
+        </Row>
       </Container>
     );
   }
