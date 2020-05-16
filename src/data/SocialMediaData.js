44078b2898a6c34b35f9e07798e8173818aae24d
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-16 19:05:04 -0400

    Adding dummy social media data

---
+++ src/data/SocialMediaData.js
@@ -0,0 +1,21 @@
+var data = {
+  content: {
+    socialMediaData: [
+      {
+        name: "LinkedIn",
+        Description: "This is my LinkedIn Account",
+        image: "Image cource to be put here",
+      },
+      {
+        name: "Twitter",
+        Description: "This is my Twitter Account",
+        image: "Image cource to be put here",
+      },
+      {
+        name: "Instagram",
+        Description: "This is my Instagram Account",
+        image: "Image cource to be put here",
+      },
+    ],
+  },
+};
