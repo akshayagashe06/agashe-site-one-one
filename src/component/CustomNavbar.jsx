f74993a06789bfaf40dbb7c32f823d8ea19cfbfb
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-15 21:18:33 -0400

    Updated css for Navbar

--- src/component/CustomNavbar.jsx
+++ src/component/CustomNavbar.jsx
@@ -14,7 +14,7 @@ class CustomNavbar extends Component {
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
+          <Button variant="outline-dark">Search</Button>
         </Form>
       </Navbar>
     );
