f908a1d07bd9e1270e174e5281a63b5fcc88bcf2
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-21 16:49:46 -0400

    Added Image selectot UI in News Page

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
-import { Jumbotron, Container, Row, Col, Image, Button } from "react-bootstrap";
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+  Image,
+  Button,
+  Form,
+} from "react-bootstrap";
 
 class News extends Component {
   render() {
     return (
-      <Container>
-        <Jumbotron>
-          <h2>This is News Page.</h2>
-          <p>Lets see how this webiste goes.</p>
-        </Jumbotron>
-      </Container>
+      <Jumbotron>
+        <Container>
+          <Form>
+            <div className="mb-3">
+              <Form.File id="formcheck-api-custom" custom>
+                <Form.File.Input isValid />
+                <Form.File.Label data-browse="Button text">
+                  Custom file input
+                </Form.File.Label>
+                <Form.Control.Feedback type="valid">
+                  You did it!
+                </Form.Control.Feedback>
+              </Form.File>
+            </div>
+            <div className="mb-3">
+              <Form.File id="formcheck-api-regular">
+                <Form.File.Label>Regular file input</Form.File.Label>
+                <Form.File.Input />
+              </Form.File>
+            </div>
+          </Form>
+        </Container>
+      </Jumbotron>
     );
   }
 }
