6fef0dd5ae9ba951b19a8b004e618af1d215701e
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-23 21:08:00 -0400

    Added Image Slider to About Page

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Jumbotron, Container, Row, Col, Image, Button } from "react-bootstrap";
 import "./About.css";
+import MyImageSlider from "./MyImageSlider.jsx";
 
 class About extends Component {
   render() {
     return (
       <Container>
+        <MyImageSlider />
         <Jumbotron>
           <h2>This is About Page.</h2>
           <p>Lets see how this webiste goes.</p>
