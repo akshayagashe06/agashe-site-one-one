2195700da512536f4014195be3939b1f51be1280
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-29 15:55:47 -0400

    Divided footer and added rough sketch

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -7,15 +7,26 @@ class Footer extends Component {
       <Card bg="dark" text="white">
         <Card.Body>
           <Row>
-            <Col xs={12} sm={4} className="person-wrapper"></Col>
-            <Col xs={12} sm={4} className="person-wrapper">
-              <h3>Akshay</h3>
-              <p>This is some text in a column</p>
+            <Col xs={12} sm={3} className="person-wrapper">
+              <h3>Important Links</h3>
+              <h6>News</h6>
+              <h6>About</h6>
             </Col>
-            <Col xs={12} sm={4} className="person-wrapper">
+            <Col xs={12} sm={3} className="person-wrapper">
+              <h3>My projects</h3>
+              <h6>Project 1</h6>
+              <h6>Project 2</h6>
+              <h6>Project 3</h6>
+            </Col>
+            <Col xs={12} sm={3} className="person-wrapper">
               <h3>Helpful links</h3>
               <h6>Formula One</h6>
               <h6>Cricket</h6>
+            </Col>
+            <Col xs={12} sm={3} className="person-wrapper">
+              <h3>Contact</h3>
+              <h6>Email</h6>
+              <h6>Phone</h6>
             </Col>
           </Row>
         </Card.Body>
