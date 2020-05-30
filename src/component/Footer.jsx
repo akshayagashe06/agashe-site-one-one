dd28eb26464e39edbf4799d0d87db77fc454c48f
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-30 19:37:30 -0400

    Added separate style class for footer title

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -9,7 +9,7 @@ class Footer extends Component {
         <Card.Body>
           <Row>
             <Col xs={12} sm={3} className="person-wrapper">
-              <h3>Important Links</h3>
+              <h3 className="footer-title-style">Important Links</h3>
               <h6>News</h6>
               <h6>About</h6>
               <ButtonGroup className="mb-2">
@@ -19,18 +19,18 @@ class Footer extends Component {
               </ButtonGroup>
             </Col>
             <Col xs={12} sm={3} className="person-wrapper">
-              <h3>My projects</h3>
+              <h3 className="footer-title-style">My projects</h3>
               <h6>Project 1</h6>
               <h6>Project 2</h6>
               <h6>Project 3</h6>
             </Col>
             <Col xs={12} sm={3} className="person-wrapper">
-              <h3>Helpful links</h3>
+              <h3 className="footer-title-style">Helpful links</h3>
               <h6>Formula One</h6>
               <h6>Cricket</h6>
             </Col>
             <Col xs={12} sm={3} className="person-wrapper">
-              <h3>Contact</h3>
+              <h3 className="footer-title-style">Contact</h3>
               <h6>Email</h6>
               <h6>Phone</h6>
             </Col>
