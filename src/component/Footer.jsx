636f756d73a068059746fb30bbded8421640284e
Akshay Agashe <akshayagashe06@gmail.com>
2020-05-30 19:32:57 -0400

    Added underline to footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Card, Col, Row, ButtonGroup, Button } from "react-bootstrap";
+import "./Home.css";
 
 class Footer extends Component {
   render() {
