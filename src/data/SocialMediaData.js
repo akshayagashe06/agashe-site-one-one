987b5297e5da67d4ad3b36a6fe6a754e8995e730
Akshay Agashe <akshayagashe06@gmail.com>
2020-06-01 18:58:36 -0400

    Added images to data source file

--- src/data/SocialMediaData.js
+++ src/data/SocialMediaData.js
@@ -4,19 +4,19 @@ var data = {
       name: "LinkedIn",
       Description:
         "This is where you can find about all my projects, my professional experience, my interest in technology and a lots more.",
-      image: "Image cource to be put here",
+      image: "../public/assets/linkedin-logo.png",
     },
     {
       name: "Twitter",
       Description:
         "This is where you can find me talk and explore my hobbies and latest news. Especially my love for F1 and cricker.",
-      image: "Image cource to be put here",
+      image: "../public/assets/twitter-logo.png",
     },
     {
       name: "Instagram",
       Description:
         "My personal photo library which I would like to share with you.",
-      image: "Image cource to be put here",
+      image: "../public/assets/instagram-logo.png",
     },
   ],
 };
