78fada628f8bd03b4cb223ce802f40094519ab16
Akshay Agashe <akshayagashe06@gmail.com>
2020-06-01 18:49:57 -0400

    Add Description for social media platforms

--- src/data/SocialMediaData.js
+++ src/data/SocialMediaData.js
@@ -2,17 +2,20 @@ var data = {
   socialMediaData: [
     {
       name: "LinkedIn",
-      Description: "This is my LinkedIn Account",
+      Description:
+        "This is where you can find about all my projects, my professional experience, my interest in technology and a lots more.",
       image: "Image cource to be put here",
     },
     {
       name: "Twitter",
-      Description: "This is my Twitter Account",
+      Description:
+        "This is where you can find me talk and explore my hobbies and latest news. Especially my love for F1 and cricker.",
       image: "Image cource to be put here",
     },
     {
       name: "Instagram",
-      Description: "This is my Instagram Account",
+      Description:
+        "My personal photo library which I would like to share with you.",
       image: "Image cource to be put here",
     },
   ],
