fcf7d072161d18c0dce62a900ce12c080fc8bd42
Akshay Agashe <akshayagashe06@gmail.com>
2020-06-09 19:34:32 -0400

    Adject styling of About page

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -16,27 +16,29 @@ import PhotoViewer from "./PhotoViewer.jsx";
 class About extends Component {
   render() {
     return (
-      <Container>
-        <MyImageSlider />
-        <Jumbotron>
-          <PhotoViewer />
-          <section className="splitter-section" />
-          <Form>
-            <h3>Write your mind out!</h3>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>
-                Put your comments here and you will hear back from me soon!
-              </Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-          </Form>
-        </Jumbotron>
+      <div>
+        <Container>
+          <MyImageSlider />
+          <Jumbotron>
+            <PhotoViewer />
+            <section className="splitter-section" />
+            <Form>
+              <h3>Write your mind out!</h3>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>
+                  Put your comments here and you will hear back from me soon!
+                </Form.Label>
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+            </Form>
+          </Jumbotron>
+        </Container>
         <Footer />
-      </Container>
+      </div>
     );
   }
 }
