cf82b0fa0e9d674332b6dcfc4c67c0efc057d504
Akshay Agashe <akshayagashe06@gmail.com>
2020-06-12 19:43:26 -0400

    Change name and add home reference

--- src/component/CustomNavbar.jsx
+++ src/component/CustomNavbar.jsx
@@ -6,7 +6,7 @@ class CustomNavbar extends Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark" className="sticky-nav">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="/">Agashe Website</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
