a5e761d9a2b51d999727e202b446136de15a64d2
Akshay Agashe <akshayagashe06@gmail.com>
2020-06-27 19:46:44 -0400

    Add filter to social media data

--- src/component/SocialColumn.jsx
+++ src/component/SocialColumn.jsx
@@ -6,16 +6,19 @@ import "./Common-style.css";
 
 class SocialColumn extends Component {
   render(props) {
+    var clickedButton = this.props.name;
     return (
       <Col xs={12} sm={4} className="person-wrapper">
-        {data.socialMediaData.map((name1, index) => (
-          <div>
-            <Image src={name1.image} roundedCircle className="profile-pic" />
-            <h3 class="socialMedia-header-style">{name1.name}</h3>
-            <p>{name1.Description}</p>
-            <p>{this.props.name}</p>
-          </div>
-        ))}
+        {data.socialMediaData
+          .filter((mediaName) => mediaName.name == clickedButton)
+          .map((name1, index) => (
+            <div>
+              <Image src={name1.image} roundedCircle className="profile-pic" />
+              <h3 class="socialMedia-header-style">{name1.name}</h3>
+              <p>{name1.Description}</p>
+              <p>{this.props.name}</p>
+            </div>
+          ))}
       </Col>
     );
   }
