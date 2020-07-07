fdc67fbd34b4907d6d795fecc1d713eae9fa1cff
Akshay Agashe <akshayagashe06@gmail.com>
2020-07-07 17:54:12 -0400

    Remove tell me about yourself from About

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -26,22 +26,6 @@ class About extends Component {
             <h3>Somethings that I like!</h3>
             <PhotoViewer />
             <section className="splitter-section-no-border" />
-            <Form className="form-style">
-              <h3>Write your mind out!</h3>
-              <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="name@example.com" />
-              </Form.Group>
-              <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>
-                  Put your comments here and you will hear back from me soon!
-                </Form.Label>
-                <Form.Control as="textarea" rows="3" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
           </Jumbotron>
         </Container>
         <Footer />
