af87889a6ab2094850b55593e8779b48bc66a6c6
Akshay Agashe <akshayagashe06@gmail.com>
2020-07-08 21:13:09 -0400

    Add blog route to navbar

--- src/component/CustomNavbar.jsx
+++ src/component/CustomNavbar.jsx
@@ -11,7 +11,7 @@ class CustomNavbar extends Component {
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/news">News</Nav.Link>
-          <Nav.Link href="/">Blogs</Nav.Link>
+          <Nav.Link href="/blog">Blogs</Nav.Link>
         </Nav>
       </Navbar>
     );
