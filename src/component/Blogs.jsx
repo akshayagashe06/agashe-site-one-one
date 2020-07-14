07ca33bc65b9a66ad359476772a82456347a2f5e
Akshay Agashe <akshayagashe06@gmail.com>
2020-07-14 18:58:55 -0400

    Add Title and Tabbed content to Blogs

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -3,13 +3,21 @@ import { Link } from "react-router-dom";
 import { Jumbotron, Container, Row, Col, Image } from "react-bootstrap";
 import "./Common-style.css";
 import Footer from "./Footer.jsx";
+import TabbedContent from "./TabbedContent.jsx";
 
 class Blogs extends Component {
   render() {
     return (
       <div>
         <Container>
-          <Jumbotron></Jumbotron>
+          <Jumbotron>
+            <h2>BLOGS</h2>
+            <p>
+              This is where all my thoughts about politics, environment, sports
+              are elaborated.
+            </p>
+            <TabbedContent />
+          </Jumbotron>
         </Container>
         <Footer />
       </div>
