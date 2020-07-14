839b6c954e9e4c70cfbd83ed34784e08549a4b39
Akshay Agashe <akshayagashe06@gmail.com>
2020-07-14 18:55:04 -0400

    Add footer and other skeleton to Blog

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Jumbotron, Container, Row, Col, Image } from "react-bootstrap";
+import "./Common-style.css";
+import Footer from "./Footer.jsx";
 
 class Blogs extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Container>
+          <Jumbotron></Jumbotron>
+        </Container>
+        <Footer />
+      </div>
+    );
   }
 }
 
