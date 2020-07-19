5b117cad3c5810818d2010db0bc93100e5938474
Akshay Agashe <akshayagashe06@gmail.com>
2020-07-19 18:32:03 -0400

    Add Hobbites description

--- src/data/CardData.js
+++ src/data/CardData.js
@@ -1,5 +1,34 @@
 var data = {
-  AboutPageCard: [],
+  AboutPageCard: [
+    // Travelling, Driving, Cooking, Watching Racing
+    {
+      name: "Travel",
+      Description:
+        "The best way means to know yourself is to explore the world.",
+      image: "../assets/linkedin-logo.png",
+      route: "/linkedin-route",
+    },
+    {
+      name: "Drive",
+      Description:
+        "This is my mode to get over my hectic weeks, nice long drives in my car is my best companion.",
+      image: "../assets/linkedin-logo.png",
+      route: "/linkedin-route",
+    },
+    {
+      name: "Cook",
+      Description: "Newly found passion to suffice my taste buds.",
+      image: "../assets/linkedin-logo.png",
+      route: "/linkedin-route",
+    },
+    {
+      name: "F1",
+      Description:
+        "I surely cannot drive an F1 car, but watching it gives me as much an adrelenane.",
+      image: "../assets/linkedin-logo.png",
+      route: "/linkedin-route",
+    },
+  ],
 
   NewPageCard: [],
 };
