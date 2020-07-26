d163febe5a5e8214bd2e2778e8c700234f3a0f20
Akshay Agashe <akshayagashe06@gmail.com>
2020-07-26 14:20:44 -0400

    Updates

---
+++ precache-manifest.2622c772805a0e3d3d67e718d8cb1372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89432f37d7a73aa9cc2b4de11f7fbd39",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "f55aa6ba5c78420ca786",
+    "url": "/myapp/static/css/main.d82611f9.chunk.css"
+  },
+  {
+    "revision": "ae1197c3f52de797b3dc",
+    "url": "/myapp/static/js/2.c38ea8ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myapp/static/js/2.c38ea8ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55aa6ba5c78420ca786",
+    "url": "/myapp/static/js/main.766138a2.chunk.js"
+  },
+  {
+    "revision": "b494a46330fac76b50f4",
+    "url": "/myapp/static/js/runtime-main.7a9c223f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
